874ae532eaca7bc209f2e5f8c457310e7806ef70
brinkerhof <murilocabral8@gmail.com>
2023-05-14 17:53:41 -0300

    fix: remove hello.ts

--- src/pages/api/hello.ts
+++
@@ -1,9 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { NextApiRequest, NextApiResponse } from 'next'
-import { middleware } from '@shared/utils/middleware'
-
-export const handler = (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({ name: 'john' })
-}
-
-export default middleware(['GET'], handler)
