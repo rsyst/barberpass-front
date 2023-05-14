1e162131fcf1705de0fbbe4f9ae6b6210ab72029
brinkerhof <murilocabral8@gmail.com>
2023-05-14 04:58:47 -0300

    fix: ta feito ai patrao arrumei a tipagem de auth

--- src/pages/api/auth/admin/index.ts
+++ src/pages/api/auth/admin/index.ts
@@ -1,57 +1,63 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { prisma } from '../../../../db/prisma'
+import { NextApiResponse } from 'next'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import cookie from 'cookie'
+import { RstNextApiRequest } from '@shared/utils/api'
+import { prisma } from '@db/prisma'
 
-interface iBarberShop {
+export interface iAuthAdminRequest {
   email: string
   password: string
 }
+
+export interface iAuthAdminResponse {
+  token: string
+}
+
 const secretKey = process.env.JWT_SECRET
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== 'POST') {
-    return res.status(405).json({ message: 'Method not allowed' })
-  }
+export const handler = async (req: RstNextApiRequest<iAuthAdminRequest>, res: NextApiResponse<iAuthAdminResponse>) => {
+  if (req.method === 'POST') {
+    const { email, password } = req.body
 
-  const { email, password } = req.body as iBarberShop
+    try {
+      const barberShop = await prisma.barber_shops.findUnique({ where: { email: email } })
 
-  try {
-    const barberShop = await prisma.barber_shops.findUnique({ where: { email: email } })
+      if (!barberShop) {
+        throw new Error('Invalid credentials')
+      }
 
-    if (!barberShop) {
-      return res.status(401).json({ message: 'Invalid credentials' })
+      const passwordMatch = await bcrypt.compare(password, barberShop.password)
+
+      if (!passwordMatch) {
+        throw new Error('Invalid credentials')
+      }
+
+      const barberShopId = barberShop.id
+
+      if (!secretKey) {
+        throw new Error('Undefined secret key')
+      }
+
+      const token = jwt.sign({ user_id: barberShopId }, secretKey)
+
+      const cookieSerialized = cookie.serialize('token', token, {
+        httpOnly: true,
+        path: '/',
+        sameSite: 'strict',
+        maxAge: 3600
+      })
+
+      res.setHeader('Set-Cookie', cookieSerialized)
+
+      return res.status(200).json({ token: token })
+    } catch (error) {
+      console.log('Error verifying admin login', error)
+      throw new Error('Error verifying admin login')
+    } finally {
+      await prisma.$disconnect()
     }
-
-    const passwordMatch = await bcrypt.compare(password, barberShop.password)
-
-    if (!passwordMatch) {
-      return res.status(401).json({ message: 'Invalid credentials' })
-    }
-
-    const barberShopId = barberShop.id
-
-    if (!secretKey) {
-      throw new Error('Undefined secret key')
-    }
-
-    const token = jwt.sign({ barberShopId: barberShopId }, secretKey)
-
-    const cookieSerialized = cookie.serialize('token', token, {
-      httpOnly: true,
-      path: '/',
-      sameSite: 'strict',
-      maxAge: 3600
-    })
-
-    res.setHeader('Set-Cookie', cookieSerialized)
-
-    return res.status(200).json({ token: token })
-  } catch (error) {
-    console.log('Error verifying barberShops login', error)
-    return res.status(500).json({ message: 'Error verifying barberShops login' })
-  } finally {
-    await prisma.$disconnect()
+  } else {
+    throw new Error('Method not allowed')
   }
 }
--- src/pages/api/auth/index.ts
+++ src/pages/api/auth/index.ts
@@ -1,44 +1,78 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { prisma } from '../../../db/prisma'
+import { NextApiResponse } from 'next'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import cookie from 'cookie'
+import { RstNextApiRequest } from '@shared/utils/api'
+import { prisma } from '@db/prisma'
 
-interface iUserAuth {
+export interface iAuthRequest {
   email: string
   password: string
   userType: string
 }
+
+export interface iAuthResponse {
+  token: string
+}
+
 const secretKey = process.env.JWT_SECRET
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== 'POST') {
-    return res.status(405).json({ message: 'Method not allowed' })
-  }
+export const handler = async (req: RstNextApiRequest<iAuthRequest>, res: NextApiResponse<iAuthResponse>) => {
+  if (req.method === 'POST') {
+    const { email, password, userType } = req.body
 
-  const { email, password, userType } = req.body as iUserAuth
+    try {
+      if (userType !== 'client') {
+        const barber = await prisma.barbers.findUnique({ where: { email: email } })
 
-  try {
-    if (userType !== 'client') {
-      const barber = await prisma.barbers.findUnique({ where: { email: email } })
+        if (!barber) {
+          throw new Error('Invalid credentials')
+        }
 
-      if (!barber) {
-        return res.status(401).json({ message: 'Invalid credentials' })
+        const passwordMatch = await bcrypt.compare(password, barber.password)
+
+        if (!passwordMatch) {
+          throw new Error('Invalid credentials')
+        }
+
+        const barberId = barber.id
+
+        if (!secretKey) {
+          throw new Error('Undefined secret key')
+        }
+
+        const token = jwt.sign({ userId: barberId, userType: userType }, secretKey)
+
+        const cookieSerialized = cookie.serialize('token', token, {
+          httpOnly: true,
+          path: '/',
+          sameSite: 'strict',
+          maxAge: 3600
+        })
+
+        res.setHeader('Set-Cookie', cookieSerialized)
+
+        return res.status(200).json({ token: token })
+      }
+      const client = await prisma.clients.findUnique({ where: { email: email } })
+
+      if (!client) {
+        throw new Error('Invalid credentials')
       }
 
-      const passwordMatch = await bcrypt.compare(password, barber.password)
+      const passwordMatch = await bcrypt.compare(password, client.password)
 
       if (!passwordMatch) {
-        return res.status(401).json({ message: 'Invalid credentials' })
+        throw new Error('Invalid credentials')
       }
 
-      const barberId = barber.id
+      const clientId = client.id
 
       if (!secretKey) {
         throw new Error('Undefined secret key')
       }
 
-      const token = jwt.sign({ userId: barberId, userType: userType }, secretKey)
+      const token = jwt.sign({ userId: clientId, userType: userType }, secretKey)
 
       const cookieSerialized = cookie.serialize('token', token, {
         httpOnly: true,
@@ -50,41 +84,13 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       res.setHeader('Set-Cookie', cookieSerialized)
 
       return res.status(200).json({ token: token })
+    } catch (error) {
+      console.log('Error verifying users login', error)
+      throw new Error('Error verifying users login')
+    } finally {
+      await prisma.$disconnect()
     }
-    const client = await prisma.clients.findUnique({ where: { email: email } })
-
-    if (!client) {
-      return res.status(401).json({ message: 'Invalid credentials' })
-    }
-
-    const passwordMatch = await bcrypt.compare(password, client.password)
-
-    if (!passwordMatch) {
-      return res.status(401).json({ message: 'Invalid credentials' })
-    }
-
-    const clientId = client.id
-
-    if (!secretKey) {
-      throw new Error('Undefined secret key')
-    }
-
-    const token = jwt.sign({ userId: clientId, userType: userType }, secretKey)
-
-    const cookieSerialized = cookie.serialize('token', token, {
-      httpOnly: true,
-      path: '/',
-      sameSite: 'strict',
-      maxAge: 3600
-    })
-
-    res.setHeader('Set-Cookie', cookieSerialized)
-
-    return res.status(200).json({ token: token })
-  } catch (error) {
-    console.log('Error verifying users login', error)
-    return res.status(500).json({ message: 'Error verifying users login' })
-  } finally {
-    await prisma.$disconnect()
+  } else {
+    throw new Error('Method not allowed')
   }
 }
