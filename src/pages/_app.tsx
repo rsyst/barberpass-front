a9f5c47f083fdd664d2f91d8f862d29ccbbe6ad8
jjustesen <johannesjustensen99@gmail.com>
2023-10-01 19:37:51 -0300

    adiciona favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,15 +6,12 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Agendou</title>
+        <title>Barberpass</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#06092B" />
-        <meta
-          name="description"
-          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
-        />
+        <meta name="description" content="Gerencie de forma fácil e rápida seus agendamentos." />
       </Head>
       <Providers>
         <Component {...pageProps} />
